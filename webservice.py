968bce0caefcdc8ff1994e046a9836b66b2d11e4
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2015-12-02 13:53:07 -0200

    Inicial import

---
+++ webservice.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from vlc import  MediaPlayer
+
+app = Flask(__name__)
+current_player = None
+
+@app.route('/start/<entry>')
+def start(entry):
+    global  current_player
+    current_player = MediaPlayer(entry)
+    current_player.play()
+
+@app.route('/pause')
+def pause(entry):
+    global  current_player
+    current_player.pause()
+
+@app.route('/stop')
+def stop(entry):
+    global  current_player
+    current_player.stop()
+    current_player = None
+
+@app.route('/play')
+def start(entry):
+    current_player.play()
+
+
+if __name__ == '__main__':
+    app.run()
