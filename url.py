ba8b0af27651e86a6596bec37a30720a18e218f3
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2015-12-09 20:22:23 -0200

    Refactoring url.

--- url.py
+++ url.py
@@ -1,5 +1,10 @@
 import config
 import os
 
-def createurl(url, entry):
-    open(os.path.join(config.url_location, entry), "w").writelines(url)
+
+
+def create(url, entry):
+    """
+    we write url to files so they are accessible through the uri
+    """
+    open(os.path.join(config.url_location, entry + ".url"), "w").writelines(url)
