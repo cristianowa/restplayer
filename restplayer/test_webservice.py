fb90ce727942502101802862b98fcd2577d7930b
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2016-01-11 08:56:13 -0200

    Moving stuff around for pypi. Unit test is broken now.

---
+++ restplayer/test_webservice.py
@@ -0,0 +1,14 @@
+import unittest
+#TODO: for now we don't run the webservice from here
+import requests
+
+class MyTestCase(unittest.TestCase):
+    def test_start(self):
+        self.assertTrue(True)
+    def test_pause(self):
+        self.assertTrue(True)
+    def test_stop(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
