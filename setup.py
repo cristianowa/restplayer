ec43257e928e3e104e08cd34be194f81fcf21e1f
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2016-01-11 09:53:08 -0200

    Adding pypi cfg files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'restplayer',
+  packages = ['restplayer'],
+  version = '0.1',
+  description = 'A Seamless Remote layer',
+  author = 'Cristiano Werner Araujo',
+  author_email = 'cristianowerneraraujo@gmail.com',
+  url = 'https://github.com/cristianowa/restplayer',
+  download_url = 'https://github.com/cristianowa/restplayer/archive/0.1.tar.gz', 
+  keywords = ['remote player', 'mp3', 'music', 'vlc'],
+  classifiers = [],
+)
